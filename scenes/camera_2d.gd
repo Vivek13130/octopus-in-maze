172537d8f729d63267c52d63db6046e6d9e11698
Vivek <96946822+Vivek13130@users.noreply.github.com>
2025-03-26 08:27:25 +0530

    added spawn and exit box
    spawn box is not currently used now . now adding random crawlers ( enemies )

--- scenes/camera_2d.gd
+++ scenes/camera_2d.gd
@@ -1,15 +1,10 @@
 extends Camera2D
-#Responsibility : 
-#control zoom in and out with limits
-#panning via w , a , s, d
-#click and drag
 
 @export var updateCanvasDynamically = false 
 
 @onready var canvas_container: Node2D = $".."
 @onready var stroke_manager: Node2D = $"../strokeManager"
 
-#const zoom_in_limit = 2 * 10e7
 
 # zooming : 
 var zoomTarget : Vector2
@@ -17,6 +12,7 @@ var zoomTarget : Vector2
 
 # panning : 
 @export var panSpeed : int = 2
+var zoom_in_limit : int = 10
 
 # click and drag : 
 var dragStartMousePos : Vector2
@@ -37,8 +33,8 @@ func simple_zoom(delta):
 	if Input.is_action_just_pressed("camera_zoom_out"):
 		zoomTarget *= 0.9
 	
-	#zoomTarget.x = clamp(zoomTarget.x, 0 , zoom_in_limit)
-	#zoomTarget.y = clamp(zoomTarget.y, 0 , zoom_in_limit)
+	zoomTarget.x = clamp(zoomTarget.x, 0 , zoom_in_limit)
+	zoomTarget.y = clamp(zoomTarget.y, 0 , zoom_in_limit)
 	
 	zoom = zoom.slerp(zoomTarget, zoomSpeed * delta)
 	if updateCanvasDynamically:
--- scenes/maze.gd
+++ scenes/maze.gd
@@ -1,24 +1,23 @@
 extends Node2D
 
-var grid_size = Manager.grid_size
 @export var cell_size: int = 120
-
-
-@export var player_scene: PackedScene
-@onready var player_container := $playerContainer 
-
 @export var wall_thickness: float = 1.0
-@onready var maze_walls_container: Node2D = $mazeWallsContainer
 @export var overlap_size: float = 20.0
 
-@export var spawn_box_size: int = 200
-@export var exit_box_size: int = 200
+
+#var grid_size = Manager.grid_size
+var grid_size = 5
+@export var player_scene: PackedScene
+
+
+@onready var player_container := $playerContainer 
+@onready var maze_walls_container: Node2D = $mazeWallsContainer
 @onready var head_to_ui: Timer = $headToUI
 @onready var time_taken: Label = $CanvasLayer/Control/VBoxContainer/TimeTaken
-
 @onready var center_container: CenterContainer = $CanvasLayer/Control/CenterContainer
 @onready var show_total_time: Label = $"CanvasLayer/Control/CenterContainer/VBoxContainer/show total time"
 @onready var heading_back: Label = $"CanvasLayer/Control/CenterContainer/VBoxContainer/heading back"
+@onready var spawn_exit_box_container: Node2D = $spawn_exit_box_container
 
 
 class Cell:
@@ -32,9 +31,14 @@ var player
 var player_body 
 
 func _ready():
-	spawn_player()
 	generate_maze()
+	
+	#spawn_spawn_box()
+	spawn_exit_box()
+	
 	convert_maze_to_walls()
+	
+	spawn_player()
 
 func _process(delta: float) -> void:
 	if(!Manager.reached_exit) : 
@@ -52,6 +56,7 @@ func _process(delta: float) -> void:
 	
 	time_taken.text = format_time(Manager.total_time)
 	check_exit_status()
+	
 	if(Manager.show_path_traveled):
 		$Line2D.points = Manager.path_of_player
 
@@ -61,70 +66,117 @@ func format_time(seconds: float) -> String:
 	return "%02d:%02d" % [mins, secs]
 
 func check_exit_status():
-	if player_body == null :
+	if player_body == null:
 		return
-	
-	var cell_x = int(player_body.global_position.x / cell_size)
-	var cell_y = int(player_body.global_position.y / cell_size)
-	if cell_x == grid_size - 1 and cell_y == grid_size - 1 and !Manager.reached_exit:
+
+	var player_pos = player_body.global_position
+	if player_pos.x > grid_size * cell_size and player_pos.y > grid_size * cell_size and !Manager.reached_exit:
 		Manager.reached_exit = true
 		$Line2D.points = Manager.path_of_player
 		$residueContainer.visible = false
 		head_to_ui.start()
-		print("timer started")
+		print("Player reached out of grid! Timer started")
+
 
 
 func spawn_player():
 	print("player spawned")
 	player = player_scene.instantiate()
 	player_body = player.get_child(2)
-	# Position player in the center of the spawn box (top-left)
+	# position player in the center of the spawn box (top-left)
 	var spawn_box_center = Vector2(cell_size/2, -cell_size )  # since spawn box will be placed at top-left
 	player.global_position = spawn_box_center
-	print(player.global_position)
-	
 	player_container.add_child(player)
 
 
-func spawn_spawn_box():
-	var spawn_box = StaticBody2D.new()
-	spawn_box.position = Vector2(-overlap_size, -overlap_size)
+func create_wall(position: Vector2, size: Vector2, color: Color) -> StaticBody2D:
+	var wall = StaticBody2D.new()
+	wall.position = position
 	
+	wall.set_collision_layer_value(1, true)
+	wall.set_collision_layer_value(2, true)
+	wall.set_collision_layer_value(3, true)
+	wall.set_collision_layer_value(4, true)
+	wall.set_collision_mask_value(1, true)
+	wall.set_collision_mask_value(2, true)
+	wall.set_collision_mask_value(3, true)
+	wall.set_collision_mask_value(4, true)
+
 	var collider = CollisionShape2D.new()
 	var shape = RectangleShape2D.new()
-	shape.size = Vector2(spawn_box_size, spawn_box_size)
+	shape.size = size
 	collider.shape = shape
-	collider.position = shape.size / 2.0
-	spawn_box.add_child(collider)
+	collider.position = size / 2.0
+	wall.add_child(collider)
+
+	# Visual
+	var sprite = Sprite2D.new()
+	var image = Image.create(int(size.x), int(size.y), false, Image.FORMAT_RGBA8)
+	image.fill(color)
+	var texture = ImageTexture.create_from_image(image)
+	sprite.texture = texture
+	sprite.position = size / 2
+	wall.add_child(sprite)
+
+	return wall
+
+
+func spawn_spawn_box():
+	var base_pos = Vector2(-cell_size - overlap_size, -cell_size - overlap_size)
+
+	# Top wall (2 parts with small opening)
+	var top_wall = create_wall(base_pos, Vector2(cell_size * 2, wall_thickness + overlap_size), Color.YELLOW)
+	#var top_wall_left = create_wall(base_pos, Vector2(cell_size , wall_thickness + overlap_size), Color.YELLOW)
+	#var top_wall_right = create_wall(base_pos + Vector2(cell_size , 0), Vector2(cell_size, wall_thickness + overlap_size), Color.YELLOW)
+
+	# Left wall (2 parts with opening)
+	var left_wall = create_wall(base_pos, Vector2(wall_thickness + overlap_size, cell_size * 2), Color.YELLOW)
 	
-	var color_rect = ColorRect.new()
-	color_rect.color = Color.DARK_GREEN
-	color_rect.size = shape.size
-	color_rect.position = Vector2.ZERO
-	color_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	spawn_box.add_child(color_rect)
-	
-	maze_walls_container.add_child(spawn_box)
+	#var left_wall_top = create_wall(base_pos, Vector2(wall_thickness + overlap_size, cell_size), Color.YELLOW)
+	#var left_wall_bottom = create_wall(base_pos + Vector2(0, cell_size ), Vector2(wall_thickness + overlap_size, cell_size ), Color.YELLOW)
+
+	# Bottom wall
+	var bottom_wall = create_wall(base_pos + Vector2(0, 2 * cell_size), Vector2(cell_size , wall_thickness + overlap_size), Color.YELLOW)
+
+	# Right wall
+	var right_wall = create_wall(base_pos + Vector2(2 * cell_size, 0), Vector2(wall_thickness+ overlap_size, cell_size ), Color.YELLOW)
+
+	spawn_exit_box_container.add_child(top_wall)
+	spawn_exit_box_container.add_child(left_wall)
+	#spawn_exit_box_container.add_child(top_wall_left)
+	#spawn_exit_box_container.add_child(top_wall_right)
+	#spawn_exit_box_container.add_child(left_wall_top)
+	#spawn_exit_box_container.add_child(left_wall_bottom)
+	spawn_exit_box_container.add_child(bottom_wall)
+	spawn_exit_box_container.add_child(right_wall)
+
+
 
 func spawn_exit_box():
-	var exit_box = StaticBody2D.new()
-	exit_box.position = Vector2((grid_size - 1) * cell_size - (exit_box_size - cell_size / 2), (grid_size - 1) * cell_size - (exit_box_size - cell_size / 2))
-	
-	var collider = CollisionShape2D.new()
-	var shape = RectangleShape2D.new()
-	shape.size = Vector2(exit_box_size, exit_box_size)
-	collider.shape = shape
-	collider.position = shape.size / 2.0
-	exit_box.add_child(collider)
-	
-	var color_rect = ColorRect.new()
-	color_rect.color = Color.DARK_RED
-	color_rect.size = shape.size
-	color_rect.position = Vector2.ZERO
-	color_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	exit_box.add_child(color_rect)
-	
-	maze_walls_container.add_child(exit_box)
+	var base_pos = Vector2((grid_size - 1) * cell_size + overlap_size , (grid_size - 1) * cell_size + overlap_size)
+
+	# Top wall (2 parts with small opening)
+	#var top_wall_left = create_wall(base_pos, Vector2(cell_size, wall_thickness + overlap_size), Color.GREEN)
+	var top_wall_right = create_wall(base_pos + Vector2(cell_size , 0) , Vector2(cell_size , wall_thickness + overlap_size), Color.GREEN)
+
+	# Left wall (2 parts with opening)
+	#var left_wall_top = create_wall(base_pos, Vector2(wall_thickness + overlap_size, cell_size), Color.GREEN)
+	var left_wall_bottom = create_wall(base_pos + Vector2(0, cell_size), Vector2(wall_thickness + overlap_size, cell_size), Color.GREEN)
+
+	# Bottom wall
+	var bottom_wall = create_wall(base_pos + Vector2(0, 2 * cell_size), Vector2(2 * cell_size + overlap_size, wall_thickness + overlap_size), Color.GREEN)
+
+	# Right wall
+	var right_wall = create_wall(base_pos + Vector2(2 * cell_size, 0), Vector2(wall_thickness + overlap_size, 2 * cell_size + overlap_size), Color.GREEN)
+
+	#spawn_exit_box_container.add_child(top_wall_left)
+	spawn_exit_box_container.add_child(top_wall_right)
+	#spawn_exit_box_container.add_child(left_wall_top)
+	spawn_exit_box_container.add_child(left_wall_bottom)
+	spawn_exit_box_container.add_child(bottom_wall)
+	spawn_exit_box_container.add_child(right_wall)
+
+
 
 
 
@@ -154,13 +206,13 @@ func generate_maze():
 		else:
 			stack.pop_back()
 	
-	# Open connection to spawn box (top-left)
+	# open connection to spawn box (top-left)
 	maze[0][0].walls["top"] = false
 	maze[0][0].walls["left"] = false
 
-	## Open connection to exit box (bottom-right)
-	#maze[grid_size - 1][grid_size - 1].walls["bottom"] = false
-	#maze[grid_size - 1][grid_size - 1].walls["right"] = false
+	## open connection to exit box (bottom-right)
+	maze[grid_size - 1][grid_size - 1].walls["bottom"] = false
+	maze[grid_size - 1][grid_size - 1].walls["right"] = false
 
 
 func get_unvisited_neighbors(pos: Vector2):
